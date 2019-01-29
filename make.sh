e992eba0090124200dc84644baee980a8ee32011
nmsoccer <nmsoccer@163.com>
2019-01-29 15:10:38 +0800

    upate make

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #use dynamic lib
-gcc -g use_slog.c -lslog -lm -o use_slog
+gcc -g use_slog.c -lslog -lm -lrt -o use_slog
 
 #use static lib
-#gcc -g use_slog.c -Wl,-Bstatic -lslog -Wl,-Bdynamic -lm -o use_slog
+#gcc -g use_slog.c -Wl,-Bstatic -lslog -Wl,-Bdynamic -lm -lrt -o use_slog
